e9edf3b8898899c7cbc6cfc3bcc61a11e1c5ee14
edilson258 <developeredilson@hotmail.com>
2023-07-14 15:57:53 +0200

    fix: incosistence background colors at auth pages

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -54,7 +54,7 @@ export default function SigninPage() {
           <span className="w-full border-t" />
         </div>
         <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-slate-50 px-2 text-muted-foreground">
+          <span className="bg-white px-2 text-muted-foreground">
             Or continue with
           </span>
         </div>
